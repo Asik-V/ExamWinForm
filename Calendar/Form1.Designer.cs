c0cf71650e8fa08841674ab1dbdd16dbf4d7d0b6
Vlad <vladasekretov@gmail.com>
2021-07-08 11:17:39 +0300

    Add button "Save notes"

--- Calendar/Form1.Designer.cs
+++ Calendar/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.bGSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bGNotesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bGPanelWithInstrumentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
@@ -128,11 +129,21 @@
             this.bGPanelWithInstrumentToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
             this.bGPanelWithInstrumentToolStripMenuItem.Text = "BG panel with instrument";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save notes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.menuStrip2);
             this.MainMenuStrip = this.menuStrip1;
@@ -160,6 +171,7 @@
         private System.Windows.Forms.ToolStripMenuItem bGSearchToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bGNotesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bGPanelWithInstrumentToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
