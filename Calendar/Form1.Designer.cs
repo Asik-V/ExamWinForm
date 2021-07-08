975f91153287afddb9ed6b5f8e26e69b506e0b86
Vlad <vladasekretov@gmail.com>
2021-07-08 11:14:30 +0300

    Add language

--- Calendar/Form1.Designer.cs
+++ Calendar/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.engToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.русToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.укрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.engToolStripMenuItem,
+            this.русToolStripMenuItem,
+            this.укрToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // engToolStripMenuItem
+            // 
+            this.engToolStripMenuItem.Name = "engToolStripMenuItem";
+            this.engToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.engToolStripMenuItem.Text = "Eng";
+            // 
+            // русToolStripMenuItem
+            // 
+            this.русToolStripMenuItem.Name = "русToolStripMenuItem";
+            this.русToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.русToolStripMenuItem.Text = "Рус";
+            // 
+            // укрToolStripMenuItem
+            // 
+            this.укрToolStripMenuItem.Name = "укрToolStripMenuItem";
+            this.укрToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.укрToolStripMenuItem.Text = "Укр";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem engToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem русToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem укрToolStripMenuItem;
     }
 }
 
